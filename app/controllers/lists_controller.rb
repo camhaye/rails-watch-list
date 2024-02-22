03335ce66820b36c7e9ee2f0cdd1bdac0a2db512
Camille Haye <ca.haye@outlook.fr>
2024-02-22 15:57:49 +0100

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,7 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmarks = Bookmark.where(list: @list)
+    @bookmark = Bookmark.new
   end
 
   def new
